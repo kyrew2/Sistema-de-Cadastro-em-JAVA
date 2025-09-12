76c709d871afe747e30aefba1f41f9abfea766b3
Pedro Henrique Franceschi <pedrohenriquekyre@gmail.com>
2025-09-12 14:46:46 -0300

    Criação de menu

--- Main.java
+++ Main.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-}
+    static Scanner scan = new Scanner(System.in);
+    static List<Veiculo> veiculos = new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.println("Controle de Cadastro de Veículos.");
+        String menu = """
+                Escolha uma das opções abaixo:
+                1 - Cadastro de Veículo
+                2 - Listar Veículos
+                3 - Excluir Veículo
+                0 - Sair
+                """;
+        int opcao;
+    }
